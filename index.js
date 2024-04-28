d0f8af33de916ada220cd6a3aebe9a9bb1532212
Aaron Bringhurst <bringhurst.aaron@gmail.com>
2024-04-28 01:07:20 -0600

    built new directory for readme'z. also built a timestamp for unique readmes so multiple readmes can exist within the directory(output). built out renderLicenseBadge funtion, built renderLicenseLink function.

--- index.js
+++ index.js
@@ -1,75 +1,91 @@
 //These bring in the modules
-const inquirer=require('inquirer');
-const fs=require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown =require('./utils/generateMarkdown');
 
 // This is an array of questions for user input
 const questions = [
-    {
-        type: "input",
-        name: "title",
-        message: "Please write the title of the project",
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "write a discripton of the project",
-    },
-    {
-        type: "input",
-        name: "tableOfContents",
-        message: "",
-    },
-    {
-        type: "input",
-        name: "installation",
-        message: "Write the instructions for installation",
-    },
-    {
-        type: "input",
-        name: "usage",
-        message: "Write the usage information",
-    },
-    {
-        type: "list",
-        name: "license",
-        message: "Chose a license of your project",
-        choices: ["MIT", "GPL 3.0", "Apache 2.0", "BSD 3-Clause", "None"],
-    },
-    {
-        type: "input",
-        name: "contributing",
-        message: "Write the contrubition guidelines",
-    },
-    {
-        type: "input",
-        name: "tests",
-        message: "Write testing instructions if any",
-    },
-    {
-        type: "input",
-        name: "questions",
-        message: "Add Contact Info",
-    },
+  {
+    type: "input",
+    name: "title",
+    message: "Please write the title of the project",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "write a discripton of the project",
+  },
+  {
+    type: "input",
+    name: "tableOfContents",
+    message: "",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Write the instructions for installation",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Write the usage information",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Chose a license of your project",
+    choices: ["MIT", "GNU GPL v3", "Apache 2.0", "BSD 3-Clause", "None"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Write the contrubition guidelines",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Write testing instructions if any",
+  },
+  {
+    type: "input",
+    name: "questions",
+    message: "Add Contact Info",
+  },
 ];
 
 // This function writes a file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
+function writeToFile(baseFileName, data) {
+    const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+    const fileName = `${baseFileName.replace('.md', '')}-${timestamp}.md`;
+    const directoryPath = path.join(__dirname, 'output');
+
+    fs.mkdir(directoryPath, { recursive: true }, (err) => { // Ensure the directory exists
         if (err) {
-            console.error('Failed to write to file:', err);
+            console.error("Failed to create directory:", err);
             return;
         }
-        console.log(`Successfully wrote to ${fileName}`);
+
+        const filePath = path.join(directoryPath, fileName); // Create the full file path
+        fs.writeFile(filePath, data, (err) => { // Write the file to the directory
+            if (err) {
+                console.error("Failed to write to file:", err);
+                return;
+            }
+            console.log(`Successfully wrote to ${filePath}`);
+        });
     });
 }
 
 // This function will initialize app
 function init() {
-    inquirer.prompt(questions).then(answers => {
-        const markdownContent = generateMarkdown(answers);
-        writeToFile("README.md", markdownContent);
-    }).catch(error => {
-        console.error('An error occurred:', error);
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      const markdownContent = generateMarkdown(answers);
+      writeToFile("README.md", markdownContent);
+    })
+    .catch((error) => {
+      console.error("An error occurred:", error);
     });
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,51 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "GNU GPL v3":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    case "Apache 2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "BSD 3-Clause":
+      return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+    case "None":
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "[MIT License](https://opensource.org/licenses/MIT)";
+    case "GNU GPL v3":
+      return "[GNU GPL v3](https://www.gnu.org/licenses/gpl-3.0)";
+    case "Apache 2.0":
+      return "[Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)";
+    case "BSD 3-Clause":
+      return "[BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause)";
+    case "None":
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "None") {
+    return "";
+}
+
+const badge = renderLicenseBadge(license);
+const link = renderLicenseLink(license);
+
+return `## License\nThis project is licensed under the ${link}.\n\n${badge}`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
